b43aa4f8bf4de0f05bd5a3acfa79b7b1a52d1c07
sandeepkumar1234 <54533323+sandeepkumar1234@users.noreply.github.com>
2021-11-17 18:22:01 +0530

    Update properties.sh

--- properties.sh
+++ properties.sh
@@ -24,5 +24,5 @@ if [ -f "$instanceFile" ]; then
    sed -i "s|$es_path|$updated_es_path|g" "$instanceFile"
    sed -i "s/$test_planId/$updated_test_planId/g" "$instanceFile"
    sed -i "s/$test_planName/$updated_test_planName/g" "$instanceFile"
-   sed -i "s/$file_Content|$updated_file_Content|g" "$instanceFile"
+   sed -i "s/$file_Content/$updated_file_Content/g" "$instanceFile"
 fi
